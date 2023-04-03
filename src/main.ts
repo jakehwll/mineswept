5638c4b5341a929c75d2d71f5bcaf4fb564f60c0
Jake <jake@hwll.me>
2023-04-03 22:59:18 +1000

    feat: refactor to class-based

--- src/main.ts
+++ src/main.ts
@@ -57,120 +57,164 @@ const Row = (children: HTMLElement[], id: string) => {
   return el;
 };
 
-const generateGrid = (width: number, height: number) => {
-  const size = width * height;
-  const field = Array(size).fill(-1);
-  const bombs = Math.round((10 / size) * size);
+class Mineswept {
+  node: Element;
+  width: number;
+  height: number;
 
-  // Add bombs!
-  const validBombLocations = [...Array(size).keys()];
-  for (var i = 0; i < bombs; i++) {
-    // Get a random location.
-    var pos = Math.round(Math.random() * (validBombLocations.length - 0) + 0);
-    // Make it a bomb.
-    field[pos] = 9;
-    // Remove it from valid locations so we don't duplicate.
-    validBombLocations.splice(pos, 1);
+  matrix: number[] | undefined
+
+  header: HTMLElement | undefined;
+  board: HTMLElement | undefined;
+  grid: HTMLElement | undefined;
+  timer: HTMLElement | undefined;
+
+  constructor(node: Element, width: number, height: number) {
+    this.node = node;
+    this.width = width;
+    this.height = height;
+
+    this.init();
   }
 
-  // Add numbers!
-  for (var n = 0; n < size; n++) {
-    let fVal = field[n];
-    if (fVal == 9) continue; // Ignore if bomb
-    var finalNumber = 0;
+  initHeader() {
+    const header = document.createElement("header");
 
-    const numbers = [
-      -1,
-      1,
-      -(width - 1),
-      -width,
-      -(width + 1),
-      width - 1,
-      width,
-      width + 1,
-    ];
+    const bombs = Digit("000");
 
-    for (const x in numbers) {
-      let num = numbers[x];
-      let i = n + num;
+    const status = document.createElement("button");
+    status.setAttribute("type", "button");
 
-      if (i < 0 || i > size) continue; // Exit if irrelevant
-      if (
-        (n + 1) % width == 0 &&
-        (num == 1 || num == width + 1 || num == -(width - 1))
-      )
-        continue;
-      if (
-        n % width == 0 &&
-        (num == -1 || num == -(width + 1) || num == width - 1)
-      )
-        continue;
-      if (field[i] == 9) finalNumber++;
+    const statusImage = document.createElement("img");
+    statusImage.setAttribute("src", "/faces/smile.svg");
+    status.appendChild(statusImage);
+
+    const timer = document.createElement("div");
+    timer.className = "timer";
+    this.timer = timer
+
+    header.appendChild(bombs);
+    header.appendChild(status);
+    header.appendChild(timer);
+    this.node.appendChild(header);
+  }
+
+  initBoard() {
+    const board = document.createElement("section");
+    board.className = "board";
+    this.node.appendChild(board);
+    this.board = board
+  }
+
+  initTimer() {
+    let time = 0;
+    this.timer!.appendChild(Digit(String(time).padStart(3, "0")));
+    setInterval(() => {
+      time = time + 1;
+      this.timer!.innerHTML = ""
+      this.timer!.appendChild(Digit(String(time).padStart(3, "0")));
+    }, 1000);
+  }
+
+  init() {
+    this.initHeader();
+    this.initBoard();
+    this.initTimer();
+    this.generateGrid(9, 12);
+  }
+
+  generateGrid = (width: number, height: number) => {
+    const size = width * height;
+    const field = Array(size).fill(-1);
+    const bombs = Math.round((10 / size) * size);
+
+    // Add bombs!
+    const validBombLocations = [...Array(size).keys()];
+    for (var i = 0; i < bombs; i++) {
+      // Get a random location.
+      var pos = Math.round(Math.random() * (validBombLocations.length - 0) + 0);
+      // Make it a bomb.
+      field[pos] = 9;
+      // Remove it from valid locations so we don't duplicate.
+      validBombLocations.splice(pos, 1);
     }
 
-    field[n] = finalNumber;
+    // Add numbers!
+    for (var n = 0; n < size; n++) {
+      let fVal = field[n];
+      if (fVal == 9) continue; // Ignore if bomb
+      var finalNumber = 0;
+
+      const numbers = [
+        -1,
+        1,
+        -(width - 1),
+        -width,
+        -(width + 1),
+        width - 1,
+        width,
+        width + 1,
+      ];
+
+      for (const x in numbers) {
+        let num = numbers[x];
+        let i = n + num;
+
+        if (i < 0 || i > size) continue; // Exit if irrelevant
+        if (
+          (n + 1) % width == 0 &&
+          (num == 1 || num == width + 1 || num == -(width - 1))
+        )
+          continue;
+        if (
+          n % width == 0 &&
+          (num == -1 || num == -(width + 1) || num == width - 1)
+        )
+          continue;
+        if (field[i] == 9) finalNumber++;
+      }
+
+      field[n] = finalNumber;
+    }
+
+    this.matrix = field
+    this.rehydrate()
+  };
+
+  rehydrate() {
+    if (!this.matrix) return
+    if (this.grid === undefined) {
+      const grid = document.createElement("div");
+      grid.className = "grid";
+      this.board?.appendChild(grid)
+      this.grid = grid
+    }
+    const rows = Array(this.height)
+      .fill("-1")
+      .map((_v, rowIndex) => {
+        return Row(
+          Array(this.width)
+            .fill("-1")
+            .map((_v, columnIndex) => {
+              const dataId = rowIndex * this.width + columnIndex;
+              return Button({
+                dataId: `${dataId}`,
+                bomb: this.matrix![dataId] === 9,
+                surrounds: this.matrix![dataId],
+              });
+            }),
+          `${rowIndex}`
+        );
+      });
+    this.grid.innerHTML = ""
+    rows.map((v) => this.grid!.appendChild(v));
   }
-
-  // Return rows!
-  const rows = Array(height)
-    .fill("-1")
-    .map((_v, rowIndex) => {
-      return Row(
-        Array(width)
-          .fill("-1")
-          .map((_v, columnIndex) => {
-            const dataId = rowIndex * width + columnIndex;
-            return Button({
-              dataId: `${dataId}`,
-              bomb: field[dataId] === 9,
-              surrounds: field[dataId],
-            });
-          }),
-        `${rowIndex}`
-      );
-    });
-
-  const grid = document.createElement("div");
-  grid.className = "grid";
-  rows.map((v) => grid.appendChild(v));
-
-  return grid;
-};
-
-const app = document.querySelector("#app");
-
-const header = document.createElement('header');
-
-const bombs = Digit('000');
-
-const status = document.createElement("button");
-status.setAttribute('type', 'button')
-
-const statusImage = document.createElement("img");
-statusImage.setAttribute('src', '/faces/smile.svg')
-status.appendChild(statusImage);
-
-const timer = document.createElement('div');
-timer.className = "timer"
-
-header.appendChild(bombs)
-header.appendChild(status)
-header.appendChild(timer)
-
-const board = document.createElement('section')
-board.className = "board"
-board.appendChild(generateGrid(9, 12))
-
-if ( app ) {
-  app.appendChild(header);
-  app.appendChild(board);
 }
 
+const canvas1 = document.querySelector("#app1");
+const canvas2 = document.querySelector("#app2");
 
-let time = 0;
-
-setInterval(() => {
-  time = time + 1;
-  timer.innerHTML = ""
-  timer.appendChild(Digit(String(time).padStart(3, "0")))
-}, 1000);
+if ( canvas1 && canvas2 ) {
+  new Mineswept(canvas1, 9, 12);
+  new Mineswept(canvas2, 9, 12);
+}
